07c2c15bc51f194f84595d280380953c642c9a1a
Daksh <165701412+Dakshckt@users.noreply.github.com>
2024-07-13 17:39:34 +0530

    Initial Commit

--- Config.py
+++ Config.py
@@ -3,5 +3,6 @@
 #? The shift should be greater than 5 and smaller than -5 for better security
 #? Shift variable should not be smaller equal -25 that may create some problems in the program 
 #* However i have handled this all condition in the program itself
+#* This is the example number you can change it as per your wish
 
 shift = 12
